46806822812165728cfd6232e9becfecb58d2bf2
DevJonathanSantos <jojo.rtd0@gmail.com>
2019-06-19 18:17:04 -0300

    Projeto final

--- Pizza/Program.cs
+++ Pizza/Program.cs
@@ -8,94 +8,111 @@ namespace Pizza
 {
     class Program
     {
+        const float VALOR_DO_SHOP = 0.80f;
+        const float VALOR_DA_PIZZA = 11.50f;
+
         static void Main(string[] args)
         {
-            double chopp = 0.80;
-            double pizza = 10.00;
-            double cobertura = 1.50;
-            int quantidade = 0;
+            int pessoas = 0;
+            int chopp = 0;
             int continuar = 1;
-
+            int banana = 0;
+            int chocolate = 0;
+            int presunto = 0;
             do
             {
-
-
-
-                Console.WriteLine("1 - para pizza de banana");
-                Console.WriteLine("2 - para pizza de chocolate");
-                Console.WriteLine("3 - para pizza de presunto");
-
-                switch (quantidade)
-                {
-                    case 1:
-                        {
-                            Console.WriteLine("pizza de banana");
-                            break;
-                        }
-                    case 2:
-                        {
-                            Console.WriteLine("pizza de chocolate");
-                            break;
-                        }
-                    case 3:
-                        {
-                            Console.WriteLine("pizza de presunto");
-                            break;
-                        }
-                    default:
-                        {
-                            Console.WriteLine("Deseja mias uma pizza? ");
-                            Console.WriteLine("1 - sim");
-                            break;
-                        }
-
-
-                }
-
-                Console.WriteLine("Deseja mias uma pizza? ");
-                Console.WriteLine("1 - sim");
-
-
                 try
                 {
-                    continuar = Convert.ToInt16(Console.ReadLine());
+                    do
+                    {
+                        Console.WriteLine("[1] - Pizza de banana");
+                        Console.WriteLine("[2] - Pizza de chocolate");
+                        Console.WriteLine("[3] - Pizza de presunto");
+                        Console.Write("Selecione a opção desejada: ");
+
+                        int opcao = 0;
+                        do
+                        {
+                            switch (opcao = Convert.ToInt16(Console.ReadLine()))
+                            {
+                                case 1:
+                                    {
+                                        Console.WriteLine("\n--- Pizza de banana ---");
+                                        Console.Write("Quantidade: ");
+                                        banana = Convert.ToInt16(Console.ReadLine());
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        Console.WriteLine("pizza de chocolate");
+                                        Console.WriteLine("Quantas ?");
+                                        chocolate = Convert.ToInt16(Console.ReadLine());
+                                        break;
+                                    }
+                                case 3:
+                                    {
+                                        Console.WriteLine("pizza de presunto");
+                                        Console.WriteLine("Quantas ?");
+                                        presunto = Convert.ToInt16(Console.ReadLine());
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        Console.WriteLine("\nEscolha uma opção valida!!");
+
+                                        Console.WriteLine("[1] - Pizza de banana");
+                                        Console.WriteLine("[2] - Pizza de chocolate");
+                                        Console.WriteLine("[3] - Pizza de presunto");
+                                        Console.Write("Selecione a opção desejada: ");
+                                        break;
+                                    }
+                            }
+                        } while (opcao != 1 && opcao != 2 && opcao != 3);
+
+                        Console.WriteLine("\nDeseja mais pizzas? ");
+                        Console.WriteLine("[1] - Sim");
+                        Console.WriteLine("[0] - Não");
+                        Console.Write("Opção: ");
+                        continuar = Convert.ToInt16(Console.ReadLine());
+
+                    } while (continuar == 1);
 
                     if (continuar == 1)
                     {
-                        Console.WriteLine("Opção invalida");
-                    }
-                    else
-                    {
-                        continuar = 1;
+                        Console.WriteLine(continuar);
                     }
 
+                    Console.WriteLine("\nGostaria de quantos chopps?");
+                    chopp = Convert.ToInt16(Console.ReadLine());
 
+                    Console.WriteLine("\nQual a quantidade de pessoas?");
+                    pessoas = Convert.ToInt16(Console.ReadLine());
+
+                    string saida = (banana != 0) ? $"{ banana.ToString() } pizzas de banna" : null;
+                    saida += (presunto != 0) ? $", {presunto.ToString()} pizzas de presunto" : null;
+                    saida += (chocolate != 0) ? $", {chocolate.ToString()} pizzas de chocolate" : null;
+                    saida += (chopp != 0) ? $", {chopp.ToString()} chopp" : null;
+
+
+                    Console.WriteLine($"Seu pedido foi {saida}");
+                    Console.WriteLine($"O total deu R$ { VALOR_DA_PIZZA * (banana + presunto + chocolate) + chopp}");
+                    Console.WriteLine($"A taxa do garçom é r${ (VALOR_DA_PIZZA * (banana + presunto + chocolate) + chopp)* 0.10}");
+                    Console.WriteLine($"O total a ser pago por pessoa é: r$ { (((VALOR_DA_PIZZA * (banana + presunto + chocolate) + chopp) * 0.10)+ VALOR_DA_PIZZA * (banana + presunto + chocolate) + chopp)/pessoas}");
+                    Console.ReadKey();
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("Opção invalida");
-                    continuar = 0;
+                    Console.WriteLine("Opção ivalida ");
+                    continuar = 1;
                 }
 
-
-                Console.ReadKey();
 
             } while (continuar == 1);
 
 
 
 
-            Console.WriteLine("Entre com o nomero de pizzas");
-            pizza = double.Parse("52.8725945", System.Globalization.CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Entre com o nomero de chopps");
-            chopp = double.Parse("52.8725945", System.Globalization.CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Entre com o nomero de pizzas");
-            cobertura = double.Parse("52.8725945", System.Globalization.CultureInfo.InvariantCulture);
-
-
-            Console.ReadLine();
 
 
         }
